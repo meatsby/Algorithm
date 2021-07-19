edd884aa5d6453de851de9ec19aaeff01286ccdc
정석윤 <meatsby@gmail.com>
2021-07-19 14:49:37 +0000

    21.07.19

---
+++ boj/12018.py
@@ -0,0 +1,23 @@
+# Yonsei TOTO
+# 우선순위 큐 공부하자
+
+n, m = map(int, input().split())
+required = []
+result = 0
+
+for i in range(n):
+    P, L = map(int, input().split())
+    mile = sorted(list(map(int, input().split())),reverse=True)
+    if P < L:
+        required.append(1)
+    else:
+        required.append(mile[L-1]+1)
+
+
+for i in sorted(required):
+    m -= i
+    if m < 0:
+        break
+    result += 1
+
+print(result)
