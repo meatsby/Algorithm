37fd6485d4ecbd1f6daccc7c75b9adc4d1ec9f03
정석윤 <meatsby@gmail.com>
2021-07-27 14:13:07 +0000

    21.07.27

--- boj/2891.py
+++ boj/2891.py
@@ -1,16 +1,28 @@
-import sys
+# 카약과 강풍
+# 정답 참고
+# 없는팀, 있는팀, 남는팀 을 -1, 0, 1 로 구분 짓고 각 요소 양 옆을 비교
 
-n, s, r = map(int, sys.stdin.readline().rstrip().split())
-damaged = list(map(int, sys.stdin.readline().rstrip().split()))
-onemore = list(map(int, sys.stdin.readline().rstrip().split()))
-count = s
+N, S, R = map(int, input().split())
+damaged = sorted(list(map(int, input().split())))
+onemore = sorted(list(map(int, input().split())))
+team = [0 for i in range(N+2)]
 
-for i in range(s):
-    for j in range(r):
-        if damaged[i]==onemore[j] or abs(damaged[i]-onemore[j])==1:
-            count -= 1
-            del onemore[j]
-            r -= 1
-            break
+for i in damaged:
+    team[i] = -1
 
-print(count)
+for i in onemore:
+    if team[i] == -1:
+        team[i] = 0
+    else:
+        team[i] = 1
+
+for i in range(1, N+2):
+    if team[i] == -1:
+        if team[i-1] == 1:
+            team[i] = 0
+            team[i-1] = 0
+        elif team[i+1] == 1:
+            team[i+1] = 0
+            team[i] = 0
+
+print(team.count(-1))
