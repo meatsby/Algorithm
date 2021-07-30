ce5eab56d02ac0d0b082d24d68fdc34b195d1761
정석윤 <meatsby@gmail.com>
2021-07-30 14:00:59 +0000

    21.07.30

--- boj/2785.py
+++ boj/2785.py
@@ -1,23 +1,16 @@
 # 체인
+# cnt > connection : 나머지를 다 못 묶은 것
+# cnt == connection : 나머지를 모두 묶고 하나 남은 자기 자신을 묶은 것
+# cnt < connection : 나머지의 갯수만큼 묶은 것
 
 N = int(input())
 chain = sorted(list(map(int, input().split())))
+connection = 0
 cnt = 0
 
-while True:
-    if len(chain) == 2:
+for i in range(N):
+    cnt = N - 1 - i
+    connection += chain[i]
+    if connection >= cnt:
+        print(cnt)
         break
-    if chain[0] == 0:
-        chain.remove(0)
-    if len(chain) == 2:
-        break
-    chain[0] -= 1
-    a = sum(chain[-2:])
-    del chain[-2:]
-    chain.append(a)
-    cnt += 1
-
-if chain[0] != 0:
-    cnt += 1
-
-print(cnt)
