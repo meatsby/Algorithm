976695838311765ad378de6477ed0af4c9b98faf
정석윤 <meatsby@gmail.com>
2021-07-30 14:59:52 +0000

    21.07.30

--- programmers/42576.py
+++ programmers/42576.py
@@ -1,12 +1,14 @@
 # Level1 완주하지 못한 선수
 
 def solution(participant, completion):
+    participant.sort()
+    completion.sort()
     
-    for i in participant[:]:
-        if i in completion:
-            participant.remove(i)
-            completion.remove(i)
+    answer = participant[-1]
     
-    answer = participant[0]
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
     
     return answer
