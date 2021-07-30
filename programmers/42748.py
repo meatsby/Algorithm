ce3a401de9a756244ea50884c04a2e9331e8d135
정석윤 <meatsby@gmail.com>
2021-07-30 15:11:47 +0000

    21.07.30

---
+++ programmers/42748.py
@@ -0,0 +1,10 @@
+# Level1 K번째수
+
+def solution(array, commands):
+    answer = []
+    
+    for p in range(len(commands)):
+        i, j, k = commands[p][0] - 1, commands[p][1], commands[p][2] - 1
+        answer.append(sorted(array[i:j])[k])
+        
+    return answer
