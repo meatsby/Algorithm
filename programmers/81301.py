b8deb2b3be2f96cd150eda56d21a46e466659c8f
정석윤 <meatsby@gmail.com>
2021-07-30 14:40:17 +0000

    21.07.30

---
+++ programmers/81301.py
@@ -0,0 +1,16 @@
+# Level1 숫자 문자열과 영단어
+
+def solution(s):
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    word = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    while True:
+        if s.isdecimal():
+            answer = int(s)
+            break
+        else:
+            for i in range(10):
+                if word[i] in s:
+                    s = s.replace(word[i], nums[i])
+                    
+    return answer
