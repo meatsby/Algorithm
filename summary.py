5e549e80ffe05d179fa635b5365a5a9679d17c55
정석윤 <meatsby@gmail.com>
2021-08-08 15:04:29 +0000

    21.08.08

--- summary.py
+++ summary.py
@@ -311,6 +311,74 @@ __init__ # 생성자, 객체는 어떤 클래스로부터 만들어지는것들,
     # 클래스 외부에서 변수 확장 가능, 그 변수는 내가 확장한 객체에만 적용이 됨
     # 파이썬에서는 외부에서 어떤객체에 추가로 변수를 만들어서 사용 가능
 
+메소드
+    class AttackUnit(Unit):
+        def __init__(self, name, hp, damage):
+            self.name = name
+            self.hp = hp
+            self.damage = damage
+
+        def attack(self, location):
+            print('{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]'.format(self.name, location, self.damage))
+
+        def damaged(self, damage):
+            print('{0} : {1} 데미지를 입었습니다.'.format(self.name, damage))
+            self.hp -= damage
+            print('{0} : 현재 체력은 {1} 입니다.'.format(self.name, self.hp))
+            if self.hp <= 0:
+                print('{0} : 파괴되었습니다.'.format(self.name))
+
+    firebat1 = AttackUnit('파이어뱃', 50, 16)
+    firebat1.attack('5시')
+    firebat1.damaged(25)
+    firebat1.damaged(25)
+
+상속
+    class Unit:  # 부모 클래스
+        def __init__(self, name, hp, speed):
+            self.name = name
+            self.hp = hp
+            self.speed = speed
+
+        def move(self, location):
+            print('[지상 유닛 이동]')
+            print('{0} : {1} 방향으로 이동합니다. [속도 {2}]'.format(self.name, location, self.speed))
+
+    class AttackUnit(Unit):  # 자식 클래스
+        def __init__(self, name, hp, speed, damage):
+            Unit.__init__(self, name, hp, speed)  # 다른 클래스에 비슷한 구문이 있을 경우 상속시켜줌
+            self.damage = damage
+
+다중 상속
+    class Flyable:
+        def __init__(self, flying_speed):
+            self.flying_speed = flying_speed
+
+        def fly(self, name, location):
+            print('{0} : {1} 방향으로 날아갑니다. [속도 {2}]'.format(name, location, self.flying_speed))
+
+    class FlyableAttackUnit(AttackUnit, Flyable):  # 여러가지 클래스를 상속
+        def __init__(self, name, hp, damage, flying_speed):
+            AttackUnit.__init__(self, name, hp, 0, damage)
+            Flyable.__init__(self, flying_speed)
+
+        def move(self, location):
+            print('[공중 유닛 이동]')
+            self.fly(self.name, location)
+
+연산자 오버로딩  # 자식클래스에서 정의한 메소드를 쓰고싶을때 메소드를 새롭게 정의해서 사용하는 것을 오버로딩이라고 함
+    vulture = AttackUnit('벌쳐', 80, 10, 20)
+    battlecruiser = FlyableAttackUnit('배틀크루저', 500, 25, 3)
+
+    vulture.move('11시')
+    battlecruiser.move('9시')
+
+
+
+
+
+
+
 
 
 
