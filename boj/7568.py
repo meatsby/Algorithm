6269caf48cdaa493cbdf853cebcf548dbe65aeeb
정석윤 <meatsby@gmail.com>
2021-08-13 15:01:52 +0000

    덩치

---
+++ boj/7568.py
@@ -0,0 +1,16 @@
+# 덩치
+# 브루트포스
+# 정답 참고
+
+N = int(input())
+people = [tuple(map(int, input().split())) for i in range(N)]
+l = []
+
+for x, y in people:
+    size = 1
+    for a, b in people:
+        if x < a and y < b:
+            size += 1
+    l.append(size)
+
+print(*l)
