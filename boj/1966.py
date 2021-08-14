bf0b3d48693fc6e8bfcfbdd08926aa614f4cc489
정석윤 <meatsby@gmail.com>
2021-08-14 15:05:44 +0000

    프린터 큐

---
+++ boj/1966.py
@@ -0,0 +1,22 @@
+# 프린터 큐
+# 정답 참고
+
+T = int(input())
+
+for i in range(T):
+    N, M = map(int, input().split())
+    imp = list(map(int, input().split()))
+    idx = list(range(N))
+    cnt = 0
+    while True:
+        if imp[0] == max(imp):
+            cnt += 1
+            if idx[0] == M:
+                print(cnt)
+                break
+            else:
+                imp.pop(0)
+                idx.pop(0)
+        else:
+            imp.append(imp.pop(0))
+            idx.append(idx.pop(0))
