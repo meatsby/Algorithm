98d20b5a970f231d5168664c6c2a74b39230107b
정석윤 <meatsby@gmail.com>
2021-09-01 13:15:54 +0000

    21.09.01

--- boj/2167.py
+++ boj/2167.py
@@ -5,12 +5,14 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*(M+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = arr[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+
 K = int(input())
 
 for _ in range(K):
-    i, j, x, y = map(lambda x: x-1, list(map(int, input().split())))
-    ans = 0
-    for a in range(i, x+1):
-        for b in range(j, y+1):
-            ans += arr[a][b]
-    print(ans)
+    i, j, x, y = map(int, input().split())
+    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
