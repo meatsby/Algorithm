864f0536accdb08f743def134ae723ebaa264c43
정석윤 <meatsby@gmail.com>
2021-09-17 14:14:36 +0000

    21.09.17

---
+++ hellocity/wk1_greedy/2.py
@@ -0,0 +1,14 @@
+# 동전 0
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coin = sorted([int(input()) for _ in range(n)], reverse=True)
+ans = 0
+
+for c in coin:
+    ans += k//c
+    k %= c
+
+print(ans)
