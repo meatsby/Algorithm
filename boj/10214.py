12bba3bbedd4c429f560de36b78a4058be15f551
정석윤 <meatsby@gmail.com>
2021-10-10 17:30:59 +0000

    21.10.11

---
+++ boj/10214.py
@@ -0,0 +1,16 @@
+# Baseball
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    y, k = 0, 0
+    for i in range(9):
+        a, b = map(int, input().split())
+        y += a
+        k += b
+    if y > k:
+        print("Yonsei")
+    elif y < k:
+        print("Korea")
+    else:
+        print("Draw")
