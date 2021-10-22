4253f1c239cfae039a983f0516995ea61b1bae2d
정석윤 <meatsby@gmail.com>
2021-10-22 11:47:42 +0000

    21.10.22

--- boj/1676.py
+++ boj/1676.py
@@ -1,14 +1,6 @@
 # 팩토리얼 0의 개수
-import math
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-ans = 0
-
-for i in range(math.factorial(n), -1, -1):
-    zeros = str(i).count("0")
-    ans += zeros
-    if zeros == 0:
-        print(ans)
-        break
+print(n//5 + n//25 + n//125)
