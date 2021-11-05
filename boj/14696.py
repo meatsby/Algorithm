c92da2dceb741365a3425ec3ba7cf1e4eea82336
정석윤 <meatsby@gmail.com>
2021-11-05 14:49:41 +0000

    21.11.05

---
+++ boj/14696.py
@@ -0,0 +1,20 @@
+# 딱지놀이
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    a, b = 0, 0
+    al = list(map(int, input().split()))
+    bl = list(map(int, input().split()))
+
+    for n in al[1:]:
+        a += 10**n
+    for n in bl[1:]:
+        b += 10**n
+    
+    if a > b:
+        print("A")
+    elif b > a:
+        print("B")
+    else:
+        print("D")
